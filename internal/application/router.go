e236508dfff590c4bdefde88111e9fbc1a7ea85c
paul.tseng <paul.tseng@advantech.com.tw>
2024-04-22 19:33:16 +0800

    feat: support manully get captcha and complete order api

--- internal/application/router.go
+++ internal/application/router.go
@@ -1,8 +1,11 @@
 package application
 
 import (
+	"fmt"
 	"log"
 	"net/http"
+	"strconv"
+	"time"
 	"tix-worker/internal/models"
 
 	"github.com/gin-gonic/gin"
@@ -10,10 +13,12 @@ import (
 
 func (app *Application) registRouter() {
 	api := app.ginEngine.Group("/api")
-	api.POST("/order/setCaptcha", app.setCaptcha)
+	api.POST("/order/setCaptcha", app.handleSetCaptcha)
+	api.POST("/order/:id/getCaptcha", app.handleGetOrderCaptcha)
+	api.POST("/order/:id/complete", app.handleCompleteOrder)
 }
 
-func (app *Application) setCaptcha(c *gin.Context) {
+func (app *Application) handleSetCaptcha(c *gin.Context) {
 	body := models.CompleteOrderBody{}
 	if err := c.Bind(&body); err != nil {
 		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
@@ -28,5 +33,66 @@ func (app *Application) setCaptcha(c *gin.Context) {
 
 	log.Printf("set captcha \"%s\" to crawler", body.Captcha)
 	crawler.SetCaptcha(body.Captcha)
+	c.JSON(http.StatusOK, nil)
+}
+
+func (app *Application) handleGetOrderCaptcha(c *gin.Context) {
+	id := c.Param("id")
+	if id == "" {
+		c.JSON(http.StatusBadRequest, gin.H{"error": "id should not be empty"})
+		return
+	}
+
+	orderId, err := strconv.Atoi(id)
+	if err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": "id's type should be number"})
+		return
+	}
+
+	order := models.Order{}
+	if err := app.db.Preload("Creator").Where("id = ?", orderId).First(&order).Error; err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": fmt.Sprintf("order not found, err: %s", err)})
+		return
+	}
+
+	if err := app.getOrderCaptcha(order); err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": fmt.Sprintf("get order captcha failed, err: %s", err)})
+		return
+	}
+
+	go func() {
+		time.Sleep(4*time.Minute + 30*time.Second)
+		if err := app.completeOrder(order); err != nil {
+			log.Printf("complete order failed, err: %s", err)
+		}
+	}()
+
+	c.JSON(http.StatusOK, nil)
+}
+
+func (app *Application) handleCompleteOrder(c *gin.Context) {
+	id := c.Param("id")
+	if id == "" {
+		c.JSON(http.StatusBadRequest, gin.H{"error": "id should not be empty"})
+		return
+	}
+
+	orderId, err := strconv.Atoi(id)
+	if err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": "id's type should be number"})
+		return
+	}
+
+	order := models.Order{}
+	if err := app.db.Preload("Creator").Where("id = ?", orderId).First(&order).Error; err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": fmt.Sprintf("order not found, err: %s", err)})
+		return
+	}
+
+	if err := app.completeOrder(order); err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": fmt.Sprintf("complete order failed, err: %s", err)})
+		return
+	}
+
 	c.JSON(http.StatusOK, nil)
 }
